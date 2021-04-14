14f0bbf372040ad867c68c974e46467a23becddb
sangavi <sangaviraj2001@gmail.com>
2021-04-14 22:47:37 +0530

    first commit by sangavi

---
+++ src/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int n=0, x, ans = 0;
+        for (x =10; x>n; x--) {
+            ans = x * 2;
+            System.out.println(ans);
+        }
+    }
+}
